493043fde4b7fd13ac282ba69838476d4ccdd930
mitchivin <mitchellivin@gmail.com>
2025-05-14 06:39:58 +1000

    Update

--- src/data/misc.js
+++ src/data/misc.js
@@ -2,15 +2,15 @@ export const wallpaperDesktop = "./assets/gui/desktop/bliss.jpg";
 export const wallpaperMobile = "./assets/gui/desktop/blissMobile.jpg";
 
 export const socialLinks = {
-  instagram: "https://www.instagram.com/mitchivsin",
+  instagram: "https://www.instagram.com/mitchivin",
   behance: "https://www.behance.net/mitch_ivin",
   github: "https://github.com/mitchivin",
   linkedin: "https://www.linkedin.com/in/mitchivin"
 };
 
 export const socialLinksAbout = [
-  { key: 'instagram', icon: '/assets/gui/start-menu/instagram.webp', label: 'Instagram' },
-  { key: 'linkedin', icon: '/assets/gui/start-menu/linkedin.webp', label: 'LinkedIn' },
-  { key: 'behance', icon: '/assets/gui/start-menu/behance.webp', label: 'Behance' },
-  { key: 'github', icon: '/assets/gui/start-menu/github.webp', label: 'Github' }
+  { key: 'instagram', icon: '../../../assets/gui/start-menu/instagram.webp', label: 'Instagram' },
+  { key: 'linkedin', icon: '../../../assets/gui/start-menu/linkedin.webp', label: 'LinkedIn' },
+  { key: 'behance', icon: '../../../assets/gui/start-menu/behance.webp', label: 'Behance' },
+  { key: 'github', icon: '../../../assets/gui/start-menu/github.webp', label: 'Github' }
 ]; 
